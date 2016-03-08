4f4e03ca2fb2a74d52b463722a8278e1c766278b
Veldmord <egor-1408@mail.ru>
2016-03-08 16:19:05 +0300

    Update Lab_3.h

--- Lab_3.h
+++ Lab_3.h
@@ -1,90 +1,53 @@
-# Lab
-
 #pragma once
 
-template<class T>
-class Tree
+
+class Bool
 {
-	struct Node {
-		T item;
-		Node *son;
-		Node *brother;
+private:
+	int nomer1;
+	int nomer2;
 
-		Node(T i, Node *s = NULL, Node *b = NULL) {
-			item = i;
-			son = s;
-			brother = b;
-
-		}
-	};
-	Node *root;
 
 public:
-	Tree() {
-		root = NULL;
+	Bool() {
+
 	}
-	~Tree() {
-		DeleteSubtree(root);
+	~Bool() {
+		
 	}
-	int GetHeight() {
-		return GetHeight(root);
-	}
-	int GetNodesOnLevel(int level) {
-		return GetNodesOnLevel(root, level);
-	}
-	void Add(const T &elem)
-		Add(root, elem);
-private:
-	void Add(Node *&node, const T &elem);
-	void DeleteSubtree(Node *node);
-	int GetHeight(Node *node);
-	int GetNodesOnLevel(Node *node, int level);
 	
+	Bool(int a, int c) //: nomer1(a), nomer2(c)
+	{
+		if (a > c)
+			cout << "True";
+		else
+			cout << "False";
+	}
+	
+	int compar(int c, int b)
+	{
+		if (c > b)
+			cout << "True";
+		else
+			cout << "False";
+	}
+
+
 };
 
-template <class T>
-void Tree<T>::DeleteSubtree(Node *node) {
-	if (node) {
-		DeleteSubtree(node->son);
-		DeleteSubtree(node->brother);
-		delete node;
-	}
-}
 
-template <class T>
-int Tree<T>::GetHeight(Node *node) {
-	if (node == NULL)
-		return 0;
-	int max = 0;
-	for (Node *current = node->son; current; curretn = current->brother) {
-		int curHeight = GetHeight(current);
-		if (curHeight > max)
-			max = curHeight;
+int main()
+{
+	Bool n1;
+	int q, w;
+	cin >> q;
+	cin >> w;
+	Bool n2(q, w);
+	int a, c;
+	cin >> a;
+	cin >> c;
 
-	}
-	return max + 1;
-}
+	n1.compar(c, a);
 
-template <class T>
-int Tree<T>::GetNodesOnLevel(Node *node, int level) {
-	if (node == NULL)
-		return 0;
-	if (level <= 0)
-		return 0;
-	return GetNodesOnLevel(node->son, level - 1) +
-		(level == 1) +
-		GetNodesOnLevel(node->brother, level);
-}
 
-template <class T>
-void Tree<T>::Add(Node *&node, const T &item) {
-	if (node == NULL) {
-		node = new Node(item);
-	}
-	else if (item < node->item) {
-		Add(node->son, item);
-	}
-	else {
-		Add(node->brother, item);
-	}
 }
